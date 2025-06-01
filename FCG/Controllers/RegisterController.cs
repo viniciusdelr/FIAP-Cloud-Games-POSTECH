050e97c7d3f8e415ef39991edefd3fa3e73fb2d7
viniciusdelr <vina.delima@outlook.com>
2025-06-01 03:02:35 -0300

    feat: register controller
    2025 06 01

---
+++ FCG/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using FCG.Data;
+using FCG.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FCG.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly DataContext _context;
+        public RegisterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<List<Users>>> Register(Users user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { mensagem = "Usuário cadastrado com sucesso!" });
+        }
+    }
+}
--- FCG/obj/Debug/net8.0/FCG.AssemblyInfo.cs
+++ FCG/obj/Debug/net8.0/FCG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd2c1f7f0f8df420d16a2ba01d160c35776509e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8985abfc60252b7a6d2bce489cd66a977ea6a1e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
