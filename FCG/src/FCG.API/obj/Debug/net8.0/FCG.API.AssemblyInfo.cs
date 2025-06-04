db94ca4717a2c6702e83d2e870c9dc177d455382
viniciusdelr <vina.delima@outlook.com>
2025-06-04 07:03:29 -0300

    feat: arquivos gerais projeto
    2025 06 04

--- FCG/src/FCG.API/obj/Debug/net8.0/FCG.API.AssemblyInfo.cs
+++ FCG/src/FCG.API/obj/Debug/net8.0/FCG.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCG.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2749f23c4b09505e46078a84106bdc6ba0326b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf1b18da92ce104e5b06526dc3b0a500d3b2fd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCG.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCG.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
